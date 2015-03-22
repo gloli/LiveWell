97af0f46d78930786fc68cb5d89fe432f5000660
M. Fay Wu <wu.mfay@gmail.com>
2015-03-21 21:20:59 -0400

    added splash screen

--- app/src/main/java/com/example/archy/livewell/MainActivity.java
+++ app/src/main/java/com/example/archy/livewell/MainActivity.java
@@ -17,9 +17,37 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 
 
 public class MainActivity extends Activity {
+
+    public static class Splash extends Activity {
+
+        /** Duration of wait **/
+        private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+        /** Called when the activity is first created. */
+        @Override
+        public void onCreate(Bundle icicle) {
+            super.onCreate(icicle);
+            setContentView(R.layout.splash_layout);
+            findViewById(android.R.id.content).postDelayed (new Runnable(){
+                @Override
+                public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                    Intent mainIntent = new Intent(Splash.this,MainActivity.class);
+                    Splash.this.startActivity(mainIntent);
+                    Splash.this.finish();
+                }
+            }, SPLASH_DISPLAY_LENGTH);
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+
+        }
+    }
 
     private Button goodButton;
     private Button badButton;
